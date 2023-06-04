3d4040731bd61a32e6121aaacba84f91c6c812a2
Toki <55911995+tokio-k@users.noreply.github.com>
2023-06-04 20:05:19 +0900

    スマホUIの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,22 @@ import type { ReactNode } from "react";
 
 export default async function Page() {
   return (
-    <div className="w-[700px] mx-auto">
+    <div className="md:w-[700px] mx-auto px-6 md:px-0">
       <article className="mt-20 flex gap-x-8 items-center">
         <img
           src="profile.png"
           alt="profile"
-          className="w-20 h-20 rounded-full"
+          className="w-14 h-14 md:w-20 md:h-20 rounded-full"
         />
         <article className="">
-          <p>
+          <p className="text-sm">
             社会人1年目のエンジニア
             <br />
             本業はメガベンで、web3界隈でも活動できるように勉強中
           </p>
         </article>
       </article>
-      <div className="flex justify-between mt-20">
+      <div className="flex justify-between mt-20 flex-wrap gap-4">
         <TwitterCard />
         <QiitaCard />
         <DevCard />
@@ -41,7 +41,7 @@ const QiitaCard = () => {
   const qiitaUrl = "https://qiita.com/toki_dev";
   return (
     <a href={qiitaUrl} target="_blank" rel="noopener">
-      <Card className="from-qiita to-qiita px-0">
+      <Card className="from-qiita to-qiita !px-0">
         <img src="logo/qiita.png" alt="Qiita logo" />
       </Card>
     </a>
@@ -51,8 +51,8 @@ const QiitaCard = () => {
 const DevCard = () => {
   return (
     <Card className="from-indigo-12 via-indigo-11 to-indigo-10">
-      <div className="text-6xl">🧑‍💻</div>
-      <p className="font-bold pt-1">App</p>
+      <div className="text-3xl md:text-6xl">🧑‍💻</div>
+      <p className="font-bold md:pt-1">App</p>
     </Card>
   );
 };
@@ -66,7 +66,7 @@ const Card = ({
 }) => {
   return (
     <div
-      className={`w-32 h-32 px-8 py-8 flex flex-col items-center justify-center rounded-xl bg-gradient-to-br cursor-pointer ${className}`}
+      className={`w-20 h-20 md:w-32 md:h-32 px-4 py-4 md:px-8 md:py-8 flex flex-col items-center justify-center rounded-xl bg-gradient-to-br cursor-pointer ${className}`}
     >
       {children}
     </div>
